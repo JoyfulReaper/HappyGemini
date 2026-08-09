c4f21f134e1a45a6b600e5f585568d9f2f59cc8a
Kyle Givler <6787154+JoyfulReaper@users.noreply.github.com>
2026-08-09 15:49:25 -0400

    Add Gemini protocol status codes

--- HappyGemini.Extensibility/GeminiStatusCode.cs
+++ HappyGemini.Extensibility/GeminiStatusCode.cs
@@ -1,8 +1,31 @@
 ﻿namespace HappyGemini.Extensibility;
 
+/// <summary>
+/// Defined Gemini protocol status codes.
+/// </summary>
 public enum GeminiStatusCode
 {
+    Input = 10,
+    SensitiveInput = 11,
+
     Success = 20,
+
+    TemporaryRedirect = 30,
+    PermanentRedirect = 31,
+
+    TemporaryFailure = 40,
+    ServerUnavailable = 41,
+    CgiError = 42,
+    ProxyError = 43,
+    SlowDown = 44,
+
+    PermanentFailure = 50,
     NotFound = 51,
-    BadRequest = 59
+    Gone = 52,
+    ProxyRequestRefused = 53,
+    BadRequest = 59,
+
+    ClientCertificateRequired = 60,
+    CertificateNotAuthorized = 61,
+    CertificateNotValid = 62
 }
