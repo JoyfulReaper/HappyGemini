c65f5e0815450857e4dadd519ba85910f246241d
Kyle Givler <6787154+JoyfulReaper@users.noreply.github.com>
2026-08-09 15:14:00 -0400

    Add HappyGemini test plugin

---
+++ HappyGemini.TestPlugin/TestPage.cs
@@ -0,0 +1,31 @@
+﻿using HappyGemini.Extensibility;
+
+namespace HappyGemini.TestPlugin;
+
+[AutoRegisterGeminiPage]
+public sealed class TestPage : IGeminiPage
+{
+    public string Path => "/plugin-test";
+
+    public async Task WriteAsync(
+        GeminiRequest request,
+        GeminiResponseWriter response,
+        CancellationToken cancellationToken)
+    {
+        await response.WriteHeaderAsync(
+            GeminiStatusCode.Success,
+            "text/gemini; charset=utf-8",
+            cancellationToken);
+
+        await response.WriteTextAsync(
+            """
+            # External plugin test
+
+            HappyGemini loaded this page from an external plugin.
+
+            If you can read this, the entire plugin pipeline works.
+
+            """.ReplaceLineEndings("\r\n"),
+            cancellationToken);
+    }
+}
